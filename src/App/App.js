c234bb141fbd5231111ada0de37aca921fb35b9c
Joey Schrader <jschrader@overstock.com>
2020-07-31 09:46:58 -0600

    set up skeleton for React Router

--- src/App/App.js
+++ src/App/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch, NavLink } from 'react-router-dom';
 
 /** components */
 import Header from './Header/Header';
+import Dashboard from './Dashboard/Dashboard';
 
 export const App = () => {
     return (
         <div>
-            <Header />
-            Hello world!!!
+            <BrowserRouter>
+                <NavLink to={'/dashboard'}>Dashboard</NavLink>
+                <Switch>
+                    <Route path="/" exact component={Header} />
+                    <Route path="/dashboard" component={Dashboard} />
+                    <Route component={Dashboard} />
+                </Switch>
+            </BrowserRouter>
         </div>
     )
 }
